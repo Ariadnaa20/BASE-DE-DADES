380684d28f1a3c2e792d8995976e71731f8315d9
ariadna <ari.palau20@gmail.com>
2024-03-18 09:45:00 +0100

    exercici 2 i 3 procediments movies_db

---
+++ UF3/EX-S05-C01/movies_db/ex2.sql
@@ -0,0 +1,21 @@
+/*
+EXERCICI 2:
+Useu la base de dades mymovies.
+Creeu un procedure nou que mostri per pantalla el títol i l'any de creació de totes les pel·lícules que en el seu títol de la pel·lícula continguin
+el text passat per paràmetre.
+
+El seu call per exemple podria ser: CALL getMoviesByName('Superman');
+
+Amb aquestes pistes, ja sabeu com s'ha de dir el procedure i quins tipus de paràmetres ha de tenir.*/
+
+DELIMITER $$
+
+CREATE PROCEDURE getMoviesByName (IN titol VARCHAR(100))
+BEGIN
+   SELECT m.name, m.year FROM Movies as m 
+   WHERE m.name LIKE CONCAT('%', titol,  '%');
+END $$
+
+DELIMITER ;
+
+
---
+++ UF3/EX-S05-C01/movies_db/ex3.sql
@@ -0,0 +1,31 @@
+/*
+EXERCICI 3:
+Useu la base de dades mymovies.
+Creeu un procedure nou que permeti a l'usuari calcular els beneficis d'una pel·lícula en concret especificant el seu id de pel·lícula.
+Els beneficis de les pel·lícules es calculen multiplicant la columna stockUnits * price.
+Els beneficis de la peli s'han de guardar en una variable de sortida.
+Aquest procedure només té un paràmetre.
+
+El seu call per exemple podria ser:
+
+DECLARE X FLOAT;
+SET X = 36;
+CALL calculateRevenue(X);
+SELECT X;
+
+Amb aquestes pistes, ja sabeu com s'ha de dir el procedure i quins tipus de paràmetres ha de tenir.
+*/
+DELIMITER $$
+CREATE PROCEDURE calculateRevenue (IN id INT, OUT revenue)
+BEGIN
+   SELECT SUM(m.stockUnits*m.price) INTO revenue
+   FROM Movies as m
+   WHERE m.id = id
+END $$
+   
+DELIMITER ;
+
+SET  @x=0; --declarem variable per que aixi guardi els valors del procediment OUT
+CALL calculateRevenue(36, @X) --cridem procediment amb valors. El 1 es el id i el 2 es on guardara el resultat del procediment
+SELECT @X as Revenue; --per recuperar el valor de la variable 
+
